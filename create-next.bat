0afc0227777c3b5ea5530372a718f67c850098d8
vixvify <137582959+vixvify@users.noreply.github.com>
2026-04-12 09:53:38 +0700

    Update create-next.bat

--- create-next.bat
+++ create-next.bat
@@ -1,6 +1,7 @@
 @echo off
 cd /d %~dp0
 
+:: รับ path
 if "%1"=="" (
   set /p TARGET=Enter target path: 
 ) else (
@@ -10,7 +11,37 @@ if "%1"=="" (
 set /p NAME=Enter project name: 
 
 cd /d %TARGET%
-npx create-next-app@latest %NAME%
 
-echo Project created at %TARGET%\%NAME%
+echo Creating Next.js app with src directory...
+call npx create-next-app@latest %NAME% --src-dir
+
+cd /d %TARGET%\%NAME%\src
+
+echo Creating folder structure...
+
+mkdir app
+mkdir components
+mkdir core
+mkdir infra
+mkdir lib
+mkdir store
+mkdir utils
+
+mkdir core\constants
+mkdir core\domain
+mkdir core\ports
+
+mkdir infra\interface
+mkdir infra\repositories
+
+echo. > core\constants\.gitkeep
+echo. > core\domain\.gitkeep
+echo. > core\ports\.gitkeep
+echo. > infra\interface\.gitkeep
+echo. > infra\repositories\.gitkeep
+
+echo.
+echo ✅ Project created at %TARGET%\%NAME%
+echo ✅ Using src/ structure
+echo ✅ Folder structure created successfully!
 pause
